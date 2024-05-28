8b4b9c3cb5ea64f05049a6b285ec98a04fc07cd6
cire3 <cire3mc@gmail.com>
2024-05-27 19:04:21 -0500

    erm wtf

--- Bukkit-Core/src/main/java/prelude/PreludePlugin.java
+++ Bukkit-Core/src/main/java/prelude/PreludePlugin.java
@@ -1,18 +1,27 @@
 package prelude;
 
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
 import prelude.adapter.VersionAdapter;
 import prelude.adapter.impl.Adapter_1_11;
 import prelude.adapter.impl.Adapter_1_16_5;
 import prelude.adapter.impl.Adapter_1_9;
 import prelude.mods.*;
 
+import java.io.File;
 import java.util.Optional;
 
 public final class PreludePlugin extends JavaPlugin {
     private static PreludePlugin instance;
     private VersionAdapter adapter = null;
+
+    @SuppressWarnings("unused")
+    private PreludePlugin(JavaPluginLoader loader, PluginDescriptionFile description,
+                          File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
 
     @Override
     public void onLoad() {
